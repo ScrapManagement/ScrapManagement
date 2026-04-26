4962797901631df09073279b552b42ec2c1f6ec0
Youusef Kelany <int.yousef@hollat.net>
2026-04-26 21:26:44 +0300

    update-auction-brodcast

--- app/Events/Auctions/NewBidPlaced.php
+++ app/Events/Auctions/NewBidPlaced.php
@@ -13,7 +13,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewBidPlaced implements ShouldBroadcast
+class NewBidPlaced implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
